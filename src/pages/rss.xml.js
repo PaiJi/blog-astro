1aa3b2d38100104a8e41e533dedb5299463053dd
JiPai <github@jipai.moe>
2024-05-25 00:42:26 +0800

    fix: fix RSS include draft

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,9 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { getAllPost } from "@utils/post";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getAllPost();
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
@@ -12,8 +12,8 @@ export async function GET(context) {
       //   ...post.data,
       title: post.data.title,
       pubDate: post.data.date,
-    //   description: post.data.description,
-    //   customData: post.data.customData,
+      //   description: post.data.description,
+      //   customData: post.data.customData,
       link: `/${post.data.permalink}/`,
     })),
   });
